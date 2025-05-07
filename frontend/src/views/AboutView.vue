7e510a218e1572ee2b17cc5b2c52d269ee21f7cf
Mohamed <msaad@goatar.com>
2025-05-07 09:34:04 +0300

    init

---
+++ frontend/src/views/AboutView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>{{ count }}</p>
+    <button @click="increment">Increment</button>
+    <button @click="decrement">Decrement</button>
+    <button @click="reset">Reset</button>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+function decrement() {
+  count.value--
+}
+function reset() {
+  count.value = 0
+}
+</script>
