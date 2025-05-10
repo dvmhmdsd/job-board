24a61150df81c18317c97e013df27d450f808b55
Mohamed <msaad@goatar.com>
2025-05-10 16:59:47 +0300

    feat (backend): make admin panel

--- backend/jobs/admin.py
+++ backend/jobs/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Applicant, Company, Job, Experience, JobApplication
+
+admin.site.register(User)
+admin.site.register(Applicant)
+admin.site.register(Company)
+admin.site.register(Job)
+admin.site.register(Experience)
+admin.site.register(JobApplication)
+
